5b1591df8a4dd0c6e5d4957aed2620865d945713
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-10-05 13:58:28 +0100

    task 0 and 1 test

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""Annotation"""
+
 
 def add(a: float, b: float) -> float:
     """Add a and b"""
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""Annotation"""
+
 
 def concat(str1: str, str2: str) -> str:
     """Returns concatenated string"""
