8a6f89163dbc8c91de5c8079e8f27db1e6c0ae96
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-10-06 09:05:54 +0100

    Completed tasks

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,6 +5,7 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Multiply float by multiplier"""
+    
     def mul(n: float) -> float:
         """Multiply my multiplier"""
         return float(n * multiplier)
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,6 +2,7 @@
 from typing import Sequence, List, Tuple, Iterable
 """Duck typing"""
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Lets duck"""
     return [(i, len(i)) for i in lst]
